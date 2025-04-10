70d9c901ad64b317c93680c6c0f3a30f9d01092f
dangdang1ing15 <shine_0512@naver.com>
2025-04-10 22:49:32 +0900

    feat : add message saving func

---
+++ HaloZone/MessageLogView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageLogView.swift
+//  HaloZone
+//
+//  Created by 성현 on 4/10/25.
+//
+
+import SwiftUI
+
+struct MessageLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageLogView()
+}
